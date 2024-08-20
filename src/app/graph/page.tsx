c2abd7b12b688218faa849f29e42332617b4fdce
alireza-sharifpour <ascode94@gmail.com>
2024-08-20 15:01:07 +0330

    separate data handling

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -16,18 +16,8 @@ import ForceGraph2D, {
 import d3 from "d3";
 import GraphHeader from "./components/GraphHeader";
 import GraphSidebar from "./components/GraphSidebar";
-import {
-  NodeWithNeighbors,
-  GraphDataWithNeighbors,
-  ICitizen,
-  Link,
-  TECHolder,
-  RegenScore,
-  TrustedSeed,
-  FarcasterConnection,
-  Node,
-  GraphData,
-} from "./types";
+import { NodeWithNeighbors, GraphDataWithNeighbors, Link, Node } from "./types";
+import { useGraphData } from "../hooks/useGraphData";
 
 const NODE_R = 10;
 
@@ -43,10 +33,10 @@ const GraphPage = () => {
       "FarcasterConnection",
     ]);
 
-  const [graphData, setGraphData] = useState<GraphData>({
-    nodes: [],
-    links: [],
-  });
+  const graphData = useGraphData(
+    selectedConnectionsCheckBox,
+    selectedNodesCheckBox
+  );
 
   const [highlightNodes, setHighlightNodes] = useState<Set<Node>>(new Set());
   const [highlightLinks, setHighlightLinks] = useState<Set<Link>>(new Set());
@@ -179,7 +169,7 @@ const GraphPage = () => {
   }, [graphData]);
 
   const filteredGraphData = useMemo(() => {
-    const filteredNodes = processedGraphData.nodes.filter(
+    const filteredNodes = graphData.nodes.filter(
       (node) =>
         selectedNodesCheckBox.includes(node.type || "") ||
         node.type === "TECHolder" ||
@@ -187,7 +177,7 @@ const GraphPage = () => {
         node.type === "TrustedSeed"
     );
 
-    const filteredLinks = processedGraphData.links.filter(
+    const filteredLinks = graphData.links.filter(
       (link) =>
         (selectedConnectionsCheckBox.includes("TECHolder") &&
           link.type === "TECHolder") ||
@@ -200,103 +190,7 @@ const GraphPage = () => {
     );
 
     return { nodes: filteredNodes, links: filteredLinks };
-  }, [processedGraphData, selectedNodesCheckBox, selectedConnectionsCheckBox]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const citizensResponse = await fetch("/data/Citizens.json");
-      const citizens = (await citizensResponse.json()) as ICitizen[];
-      const tecHoldersResponse = await fetch("/data/TECHolders.json");
-      const tecHolders = (await tecHoldersResponse.json()) as TECHolder[];
-      const regenScoresResponse = await fetch("/data/RegenScore.json");
-      const regenScores = (await regenScoresResponse.json()) as RegenScore[];
-      const trustedSeedResponse = await fetch("/data/TrustedSeed.json");
-      const trustedSeeds = (await trustedSeedResponse.json()) as TrustedSeed[];
-      const farcasterConnectionsResponse = await fetch(
-        "/data/CitizensFarcasterConnections.json"
-      );
-      const farcasterConnections =
-        (await farcasterConnectionsResponse.json()) as FarcasterConnection[];
-
-      const citizenNodes: ICitizen[] = citizens.map((citizen) => ({
-        ...citizen,
-        type: "citizens",
-      }));
-
-      const TecHolderNode: Node = {
-        id: "TECHolder",
-        type: "TECHolder",
-      };
-
-      const RegenScoreNode: Node = {
-        id: "RegenScore",
-        type: "RegenScore",
-      };
-
-      const TrustedSeedNode: Node = {
-        id: "TrustedSeed",
-        type: "TrustedSeed",
-      };
-
-      const nodes: Node[] = [
-        ...citizenNodes,
-        TecHolderNode,
-        RegenScoreNode,
-        TrustedSeedNode,
-      ];
-
-      const links: Link[] = [];
-
-      // Iterate through each citizen and check if they exist in tecHolders
-      citizens.forEach((citizen) => {
-        const matchingTEC = tecHolders.find(
-          (holder) => holder.id.toLowerCase() === citizen.id.toLowerCase()
-        );
-        const matchingRegenScore = regenScores.find(
-          (score) => score.address.toLowerCase() === citizen.id.toLowerCase()
-        );
-        const matchingTrustedSeed = trustedSeeds.find(
-          (trustedSeed) =>
-            trustedSeed?.id.toLowerCase() === citizen.id.toLowerCase()
-        );
-
-        if (matchingTEC) {
-          links.push({
-            source: citizen.id,
-            target: TecHolderNode.id,
-            type: "TECHolder",
-          });
-        }
-        if (matchingRegenScore) {
-          links.push({
-            source: citizen.id,
-            target: RegenScoreNode.id,
-            type: "RegenScore",
-          });
-        }
-        if (matchingTrustedSeed) {
-          links.push({
-            source: citizen.id,
-            target: TrustedSeedNode.id,
-            type: "TrustedSeed",
-          });
-        }
-      });
-
-      // Add FarcasterConnection links
-      farcasterConnections.forEach((connection) => {
-        links.push({
-          source: connection.source,
-          target: connection.target,
-          type: "FarcasterConnection",
-        });
-      });
-
-      setGraphData({ nodes, links });
-    };
-
-    fetchData();
-  }, [selectedConnectionsCheckBox, selectedNodesCheckBox]);
+  }, [graphData, selectedNodesCheckBox, selectedConnectionsCheckBox]);
 
   useEffect(() => {
     setTimeout(() => {
