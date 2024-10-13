eb0fe16bfdfb966e737b98933d83f0dc6973632c
alireza-sharifpour <ascode94@gmail.com>
2024-10-13 14:57:54 +0330

    update root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,17 @@
-import { createTRPCRouter } from "./trpc";
+import { createCallerFactory, createTRPCRouter } from "./trpc";
 import { graphRouter } from "./routers/graph";
 
 export const appRouter = createTRPCRouter({
-  graph: graphRouter,
+  graph: graphRouter
 });
 
 export type AppRouter = typeof appRouter;
+
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+export const createCaller = createCallerFactory(appRouter);
