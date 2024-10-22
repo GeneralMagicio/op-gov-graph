8b568c7da8e37ea0c70cb52269900bdd43017f31
alireza-sharifpour <ascode94@gmail.com>
2024-10-22 13:31:37 +0330

    add new routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,14 @@
 import { createCallerFactory, createTRPCRouter } from "./trpc";
 import { graphRouter } from "./routers/graph";
+import { farcasterRouter } from "./routers/farcaster";
+import { badgeHolderRouter } from "./routers/badgeHolder";
+import { nodeRouter } from "./routers/node";
 
 export const appRouter = createTRPCRouter({
-  graph: graphRouter
+  node: nodeRouter,
+  badgeHolder: badgeHolderRouter,
+  graph: graphRouter,
+  farcaster: farcasterRouter
 });
 
 export type AppRouter = typeof appRouter;
