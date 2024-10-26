c5d4c7da9d27c5be973cb24ee9f6ed41580daa38
alireza-sharifpour <ascode94@gmail.com>
2024-10-26 13:17:50 +0330

    update env

--- src/env.js
+++ src/env.js
@@ -8,18 +8,18 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    AIRSTACK_API_KEY: z.string().min(1),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development")
   },
-
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    // Add any client-side env vars here if needed
   },
 
   /**
@@ -28,8 +28,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    AIRSTACK_API_KEY: process.env.AIRSTACK_API_KEY,
     NODE_ENV: process.env.NODE_ENV
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
