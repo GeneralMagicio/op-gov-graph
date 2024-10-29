e05c7e4a5e7caf8d0d15a81a77c87dd9892d4a2a
Alireza Sharifpour <48887817+alireza-sharifpour@users.noreply.github.com>
2024-10-29 14:45:44 +0330

    add connection for production db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,23 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-
 import { env } from "@/env";
-import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
+const connectionString = env.DATABASE_URL;
+
+// Database connection configuration
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const conn =
+  globalForDb.conn ??
+  postgres(connectionString, {
+    ssl: "require",
+    max: 1,
+    idle_timeout: 20,
+    connect_timeout: 10
+  });
 
-export const db = drizzle(conn, { schema });
+if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn);
