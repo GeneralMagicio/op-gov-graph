ccb4a7968e0681cd8df315730ae72b687c3edad0
Alireza Sharifpour <ascode94@gmail.com>
2024-11-20 13:56:13 +0330

    update type

--- src/app/graph/types/index.ts
+++ src/app/graph/types/index.ts
@@ -21,6 +21,17 @@ export enum NodeType {
   // RefiDAO = "RefiDAO", // Uncomment if needed in the future
 }
 
+export type TopIssue = {
+  type: string;
+  value: string;
+};
+
+export type VotingPower = {
+  total: string;
+  direct: string;
+  advanced: string;
+};
+
 export interface RegenPOAPHolder {
   Collection: string;
   Count: number;
